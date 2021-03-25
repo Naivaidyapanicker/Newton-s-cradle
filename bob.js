1faf067de94f60574021d4bc354bf09d75c91eb9
Naivaidyapanicker <76810771+Naivaidyapanicker@users.noreply.github.com>
2021-03-25 12:42:41 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,23 @@
+class Bob {
+  constructor(x, y,radius) {
+    var options = {
+        isSatic:false,
+        'restitution':1,
+        'friction':0,
+        'density':7.8
+    }
+    
+    this.body = Bodies.circle(x, y, 25, options); 
+    this.radius=25 
+    World.add(world, this.body);
+
+    console.log(this.body);
+  }
+  display(){
+    push();
+    ellipseMode(RADIUS);
+    fill(254,0,255);
+    ellipse(this.body.position.x, this.body.position.y, 25, 25);
+    pop();
+  }
+};
